80a67c20c74cae51df877c8e76eb31ec8932e3d8
Heldenglanz <Heldenglanz@gmail.com>
2022-06-07 20:58:09 +0300

    8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
 
+const superForm = document.querySelector('.login-form')
+console.log(superForm)
+
+function formChanging(e) {
+    e.preventDefault();
+    const { email, password } = e.target.elements
+    const mailName = email.value;
+    const pwdName = password.value;
+    if (mailName.trim() === ''|| pwdName === '') {
+        alert('все поля должньі бьіть заполненьі')
+    }
+    const object = {
+        [email.name] : mailName,
+        [password.name] : pwdName,
+    }
+ 
+    console.log(object)
+    e.currentTarget.reset()
+}
+superForm.addEventListener('submit', formChanging)
