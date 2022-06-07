aec500cb274447c80a638d9270f6d8ee69878789
Heldenglanz <Heldenglanz@gmail.com>
2022-06-07 21:48:06 +0300

    9+

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,19 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
 }
+const myBody = document.body;
+const myButton = document.querySelector('.change-color')
+const spanner = document.querySelector('.color')
+
+function superColorChanger() {
+const myColor = getRandomHexColor()
+  myBody.style.background = myColor;
+  spanner.textContent = myColor;
+}
+
+
+
+
+
+
+myButton.addEventListener('click', superColorChanger)
